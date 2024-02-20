00ec7b93dbbf508ec0259530414dcb30f4a8d073
Le Van Dat <72507371+lvdat@users.noreply.github.com>
2024-02-20 19:19:27 +0700

    fix path make action run fail

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -94,7 +94,7 @@ const config = {
             items: [
               {
                 label: 'Tài liệu',
-                to: '/docs/intro',
+                to: '/docs/start',
               },
             ],
           },
